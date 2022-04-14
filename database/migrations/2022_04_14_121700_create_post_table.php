f493c2ebd4f5ef1c2d9402a6cf841b4f08110022
sumayah swaib <>
2022-04-14 15:30:16 +0300

    database commite

---
+++ database/migrations/2022_04_14_121700_create_post_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->text('title',500);
+            $table->text('body');
+            $table->integer('user_id');
+            $table->text('photo');
+            $table->integer('category_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post');
+    }
+}
--- resources/views/layouts/PageLayout.blade.php
+++ resources/views/layouts/PageLayout.blade.php
@@ -5,18 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>goprint @yield('title')</title>
+    <link rel="stylesheet" href="bootstrap.css">
 </head>
-<body>
-    <h1>@yield('header')</h1>
- <ul>
+<body class="container" >
+    <div class="row">
+    
+    <div class="col-md-4 pading up " >
+        <h1>@yield('header')</h1>
 
-        <li><a href="/"></a>home page</li>
-        <li><a href="/about"></a>about us page</li>
-        <li><a href="/contact"></a>contact us page</li>
+        <ul>
 
-</ul>
+            <li><a href="/"></a>home page</li>
+            <li><a href="/about"></a>about us page</li>
+            <li><a href="/contact"></a>contact us page</li>
+    
+    </ul>
+    </div>
+    <div class=" col-md-8 border-left" >
 
-    <p>@yield('content')<p>
-        <h4>@ Copy right Reserved</h4>
+        <p>@yield('content')<p>
+            <h4>@ Copy right Reserved</h4>
+    
+    </div>
+    </div>
+    
+    
+ 
+
+    
 </body>
 </html>
